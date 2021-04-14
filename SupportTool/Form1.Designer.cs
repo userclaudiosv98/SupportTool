b536982a1578aaa8247a92122f6d8376b6207067
Claudio <claudiosousav998@gmail.com>
2021-04-13 22:43:35 -0300

    - Adicionada função de capturar o IP público do usuário; - Adicionada função de capturar o IP interno do usuário; - Adicionado link de redirecionamento no linkLabel;

--- SupportTool/Form1.Designer.cs
+++ SupportTool/Form1.Designer.cs
@@ -31,6 +31,8 @@ namespace SupportTool
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // linkLabel1
@@ -48,6 +50,30 @@ namespace SupportTool
             this.linkLabel1.VisitedLinkColor = System.Drawing.Color.Red;
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Location = new System.Drawing.Point(95, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(285, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Clique aqui para identificar seu IP Púiblico";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.Location = new System.Drawing.Point(95, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(285, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clique aqui para identificar seu IP Interno";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -56,6 +82,8 @@ namespace SupportTool
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(491, 331);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.linkLabel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -71,6 +99,8 @@ namespace SupportTool
         #endregion
 
         private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- SupportTool/Form1.cs
+++ SupportTool/Form1.cs
@@ -1,19 +1,44 @@
 ﻿using System.Windows.Forms;
 using System.Diagnostics;
+using System.Net;
+using System.IO;
 
 namespace SupportTool
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
+        public Form1(){
             InitializeComponent();
         }
 
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            MessageBox.Show("Clicou no link.");
-            Process.Start("https://www.youtube.com/");
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e){
+            Process.Start("https://www.youtube.com/watch?v=bUTK1qjL_kY&list=RDbUTK1qjL_kY&start_radio=1");
+        }
+
+        private void button1_Click(object sender, System.EventArgs e){
+            var endPoint = "https://api.ipify.org/";
+            var request = (HttpWebRequest)WebRequest.Create(endPoint);
+            var response = (HttpWebResponse)request.GetResponse();
+            var resStream = response.GetResponseStream();
+            var streamReader = new StreamReader(resStream);
+            button1.Text = $"IP Público: {streamReader.ReadToEnd()}";
+        }
+
+        private void button2_Click(object sender, System.EventArgs e){
+            IPHostEntry host;
+            var localIP = string.Empty;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach(IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily.ToString().Equals("InterNetwork"))
+                {
+                    localIP = ip.ToString();
+                    break;
+                }
+            }
+
+            button2.Text = $"IP Interno: {localIP}";
         }
     }
 }
