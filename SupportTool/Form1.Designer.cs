ea3033e1342e2fa39d08d2670937c65463a91543
Claudio <claudiosousav998@gmail.com>
2021-04-16 00:21:26 -0300

    Ajuste no copyright

--- SupportTool/Form1.Designer.cs
+++ SupportTool/Form1.Designer.cs
@@ -47,10 +47,10 @@ namespace SupportTool
             this.linkLabel1.LinkColor = System.Drawing.Color.Black;
             this.linkLabel1.Location = new System.Drawing.Point(383, 309);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(96, 13);
+            this.linkLabel1.Size = new System.Drawing.Size(99, 13);
             this.linkLabel1.TabIndex = 0;
             this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "©iClaudioSolutions";
+            this.linkLabel1.Text = "© iClaudioSolutions";
             this.linkLabel1.VisitedLinkColor = System.Drawing.Color.Red;
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
